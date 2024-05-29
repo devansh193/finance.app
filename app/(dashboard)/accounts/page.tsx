f977f9af6bc137bfe6e1a0d8ef93b95072d783c5
devansh <devanshverma193@gmail.com>
2024-05-29 23:39:47 +0530

    skeleton

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -2,16 +2,34 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 
 import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
   const accountQuery = useGetAccounts();
   const accounts = accountQuery.data || [];
+
+  if(accountQuery.isLoading){
+    return(
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24 py-12">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-28"/>
+            <CardContent>
+              <div className="h-[500px] w-full flex items-center justify-center">
+                <Loader2 className="size-6 text-slate-300 animate-spin"/>
+              </div>
+            </CardContent>
+          </CardHeader>
+        </Card>  
+      </div>
+    )
+  }
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24 py-12">
