df1afc812c3a3f6bb50f77c190f3b092736924ae
devansh <devanshverma193@gmail.com>
2024-07-13 00:54:59 +0530

    transaction-page

--- components/welcomeMsg.tsx
+++ components/welcomeMsg.tsx
@@ -6,7 +6,7 @@ export const WelcomeMsg = () =>{
     const {user, isLoaded} = useUser();
     return(
         <div className="space-y-2 mb-4">
-            <h2 className="text-2xl lg:text-4xl text-white font-medium ">Welcome Back {isLoaded? ", ": " "}{user?.firstName} 👋🏻</h2>
+            <h2 className="text-2xl lg:text-4xl text-white font-medium ">Welcome Back {isLoaded? ",": " "}{user?.firstName} 👋🏻</h2>
             <p className="text-sm lg:text-base text-[#89b6fd]">Track your finances</p>
         </div>
     );
