4a263a0549cd0c1709d18697dd0ffeb08bf82746
devansh <devanshverma193@gmail.com>
2024-07-28 17:48:58 +0530

    dashboard-charts

--- components/data-chart.tsx
+++ components/data-chart.tsx
@@ -2,6 +2,7 @@
 
 import { useGetSummary } from "@/features/summary/api/use-get-summary";
 import { Chart } from "./chart";
+import { SpendingPie } from "./spending-pie";
 
 export const DataChart = () => {
   const { data, isLoading } = useGetSummary();
@@ -10,9 +11,12 @@ export const DataChart = () => {
   }
 
   return (
-    <div className="grid gird-cols-1 lg-grid-cold-6 gap-8">
-      <div className="col-span-1 lg:col-span-3 fl:col-span-4">
+    <div className="grid gird-cols-1 lg-grid-cols-6 gap-8">
+      <div className="col-span-1 lg:col-span-3 xl:col-span-4">
         <Chart data={data?.days} />
+      </div>
+      <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+        <SpendingPie data={data?.categories}/>
       </div>
     </div>
   );
---
+++ components/spending-pie.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { FileSearch, PieChart, Radar, Target } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { AreaVariant } from "./area-variant";
+import { BarVariant } from "./bar-variant";
+import { LineVariant } from "./line-variant";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { AreaChart } from "recharts";
+import { PieVariant } from "./pie-variant";
+
+type Props = {
+  data?: {
+    name: string;
+    value: number;
+
+  }[];
+};
+
+export const SpendingPie = ({ data = [] }: Props) => {
+  const [chartType, setChartType] = useState("pie");
+  const onTypeChange = (type: string) => {
+    setChartType(type);
+  };
+
+  return (
+    <Card className="boarder-none drop-shadow-sm">
+      <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
+        <CardTitle className="text-xl line-clamp-1">Categories</CardTitle>
+        <Select 
+          defaultValue={chartType}
+          onValueChange={onTypeChange}>
+        <SelectTrigger className="lg:w-auto h-9 rounded-md px-3">
+            <SelectValue placeholder="Chart type" />
+        </SelectTrigger>
+
+        <SelectContent>
+            <SelectItem value="pie">
+              <div className="flex items-center">
+                <PieChart className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Pie chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="radar">
+              <div className="flex items-center">
+                <Radar className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Radar chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="radial">
+              <div className="flex items-center">
+                <Target className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Radial chart</p>
+              </div>
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardContent>
+        {data.length === 0 ? (
+          <div className="flex flex-col gap-y-4 items-center justify-center h-[350px] w-full">
+            <FileSearch className="size-6 text-muted-foreground" />
+            <p className="text-muted-foreground text-sm">
+              No data for this period
+            </p>
+          </div>
+        ) : (
+            <>
+            {chartType === "pie" && <PieVariant data={data}/>}
+            {chartType === "radar" && <BarVariant data={data}/>}
+            {chartType === "radial" && <AreaVariant data={data}/>}
+            </>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
